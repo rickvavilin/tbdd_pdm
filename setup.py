d0c8ed975d79385f05512924badce1e56ed1273e
Aleksandr Vavilin <vavilin@tbdd.ru>
2018-09-07 20:46:59 +0500

    remove unused file; reformat setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-__author__ = 'Aleksandr Vavilin'
 from distutils.core import setup
 import os
 import subprocess
-import sys
+__author__ = 'Aleksandr Vavilin'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     build = (('BUILD_NUMBER' in os.environ) and os.environ['BUILD_NUMBER'] or "M")
     branch = (('GIT_BRANCH' in os.environ) and os.environ['GIT_BRANCH'] or "")
     proc = subprocess.Popen(['git', 'rev-parse', '--short=8', 'HEAD'], stdout=subprocess.PIPE)
@@ -14,7 +13,6 @@ if __name__=='__main__':
     package_name = 'tbdd-pdm'
     versionstr = '0.1.{}-{}'.format(build, branch)
     fullversionstr = '0.1.{}-{} {}'.format(build, branch, commit)
-
 
     d = setup(
         name=package_name,
